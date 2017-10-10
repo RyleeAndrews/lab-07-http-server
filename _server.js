cced617663d1ac6c003c4a078a03d7b734e027b7
RyleeAndrews <ryleeandrews73@gmail.com>
2017-10-09 22:36:22 -0700

    server constructed now working on testing to see if it works

--- _server.js
+++ _server.js
@@ -16,6 +16,22 @@ const server = module.exports = http.createServer( (req,res) => {
   }else if(req.method === 'GET' && req.url.pathname === '/cowsay?text={message}'){
     sendResponse(res,200, '<pre> cowsay.say({text: req.query.text}) </pre> ');
   }else if(req.method === 'POST' && req.url.pathname === '/api/cowsay'){
-    sendResponse
+    let body = '';
+    res.on('data', function(data){
+      body += data.toString();
+    });
+    res.on('end', function(){
+      let json;
+      try {
+        json = JSON.parse(body);
+      } catch(e) {
+        return sendResponse(res,400,{'Content-Type': 'application/json'},{"error":"invalid request: text query required"});
+      }
+      console.log(json);
+      sendResponse(res,200,{'Content-Type': 'application/json'},{ "content": "<cowsay cow text>"});
+    });
+  }else{
+    sendResponse(res,400,{'Content-Type': 'application/json'},{ "error": "invalid request: text query required"});
   }
-});
+}
+);
---
+++ server.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const server = require('./_server.js');
+server.listen(3000);
