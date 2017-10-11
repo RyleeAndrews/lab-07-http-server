226836cd059b39be5f721b5b3370c5e4ac038785
RyleeAndrews <ryleeandrews73@gmail.com>
2017-10-10 20:25:59 -0700

    get / working, everything is else is returning errors ; ;

--- _server.js
+++ _server.js
@@ -14,24 +14,24 @@ const server = module.exports = http.createServer( (req,res) => {
   if(req.method === 'GET' && req.url.pathname === '/'){
     sendResponse(res,200,'<a href="/cowsay">cowsay</a> <main> project description </main> ');
   }else if(req.method === 'GET' && req.url.pathname === '/cowsay?text={message}'){
-    sendResponse(res,200, '<pre> cowsay.say({text: req.query.text}) </pre> ');
+    sendResponse(res,200,'<pre> cowsay.say({text: req.query.text}) </pre> ');
   }else if(req.method === 'POST' && req.url.pathname === '/api/cowsay'){
     let body = '';
-    res.on('data', function(data){
+    req.on('data', function(data){
       body += data.toString();
     });
-    res.on('end', function(){
+    req.on('end', function(){
       let json;
       try {
         json = JSON.parse(body);
       } catch(e) {
-        return sendResponse(res,400,{'Content-Type': 'application/json'},{"error":"invalid request: text query required"});
+        return sendResponse(res,400,'{"error":"invalid request: body required"}');
       }
       console.log(json);
-      sendResponse(res,200,{'Content-Type': 'application/json'},{ "content": "<cowsay cow text>"});
+      sendResponse(res,200,'{ "content": "<cowsay cow text>"}');
     });
   }else{
-    sendResponse(res,400,{'Content-Type': 'application/json'},{ "error": "invalid request: text query required"});
+    sendResponse(res,400,'{ "error": "invalid request: text query required"}');
   }
 }
 );
